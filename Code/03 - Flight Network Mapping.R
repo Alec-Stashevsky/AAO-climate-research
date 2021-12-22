240cc8174d8a6014ff1dcb59fc3b1b1631506f25
Alec Stashevsky <alec@alecstashevsky.com>
2021-12-21 19:16:39 -0800

    Add additional locations for steps 03 - 04

--- Code/03 - Flight Network Mapping.R
+++ Code/03 - Flight Network Mapping.R
@@ -24,12 +24,13 @@ path.viz <- "~/AAO-climate-research/Visualizations/"
 
 # AAO Geo-Unique Frequency Data
 aao <- readRDS(paste0(path.in, "AAO_GEODISTANCE.RDs"))
+conventions <- readRDS(paste0(path.in, "AAO_CONVENTIONS.RDs"))
 
 
 # Plot Attendance Map -----------------------------------------------------
 
 pdf(
-  file = paste0(path.viz, "AAO Flight Networks.pdf"),
+  file = paste0(path.viz, "AAO Flight Networks All.pdf"),
   onefile = TRUE,
   width = 8,
   height = 6
@@ -53,36 +54,18 @@ points(
   )
 
 
-# Plot Flight Networks ----------------------------------------------------
-
-# Get coordinates for destination airport SFO
-SFO <- as.numeric(aao[`Airport Code` == "SFO", .(Longitude, Latitude)])
-
-# Plot a map of the united states
-map("world", col = "grey20", fill = TRUE, bg = "black", lwd = 0.1)
-
-title(
-  main = "AAO San Francisco Meeting Flight Network",
-  col.main = "white", col.sub = "white"
-)
-
-# Plot attendee origins (proxy via airports locations)
-points(
-  x = aao[drive.SFO == 0]$Longitude,
-  y = aao[drive.SFO == 0]$Latitude,
-  pch = 20,
-  cex = log(aao[drive.SFO == 0]$Frequency) / (max(log(aao[drive.SFO == 0]$Frequency)) * 2),
-  col = rgb(red = 1, green = .69, blue = 0, alpha = 0.3)
-)
-
-# Parameterize color of lines
-col.1 <- adjustcolor("orange red", alpha = 0.10)
-col.2 <- adjustcolor("orange", alpha = 0.15)
-edge.pal <- colorRampPalette(c(col.1, col.2), alpha = TRUE)
-edge.col <- edge.pal(100)
-
-
-flight_network <- function(data, destination, rows = "default", endpts = TRUE) {
+# Build Flight Network Plotter -------------------------------------------
+flight_network <- function(
+  data,
+  destination,
+  main = "",
+  sub = "",
+  point.col = rgb(red = 1, green = .69, blue = 0, alpha = 0.3),
+  edge.col = edge.pal(100),
+  edge.ind.adj = 50,
+  lwd.adj = 200,
+  rows = "default",
+  endpts = TRUE) {
 
   if (rows == "default") {
     rows <- c(1, nrow(data))
@@ -90,9 +73,29 @@ flight_network <- function(data, destination, rows = "default", endpts = TRUE) {
     stop("`rows` must be a numeric vector of length 2")
   }
 
+  # Plot a map of the united states
+  map("world", col = "grey20", fill = TRUE, bg = "black", lwd = 0.1)
+
+  # Add title
+  title(
+    main = main,
+    sub = sub,
+    col.main = "white", col.sub = "white"
+  )
+
+  # Plot origin points
+  points(
+    x = data[drive.SFO == 0]$Longitude,
+    y = data[drive.SFO == 0]$Latitude,
+    pch = 20,
+    cex = log(data[drive.SFO == 0]$Frequency) / (max(log(data[drive.SFO == 0]$Frequency)) * 2),
+    col = point.col
+  )
+
+  # Plot Geodesic Arcs
   for (i in rows[1]:rows[2]) {
 
-    # Build geodesic distance to SFO Airport
+    # Build geodesic distance to destination
     routes <- gcIntermediate(
       c(data$Longitude[i], data$Latitude[i]),
       destination,
@@ -101,16 +104,81 @@ flight_network <- function(data, destination, rows = "default", endpts = TRUE) {
       breakAtDateLine = TRUE,
       sp = TRUE)
 
-    edge.ind <- round(50 * data$Frequency[i] / max(data$Frequency[i]))
+    edge.ind <- round(edge.ind.adj * data$Frequency[i] / max(data$Frequency[i]))
 
-    lines(routes, col = edge.col[edge.ind], lwd = edge.ind / 200)
+    lines(routes, col = edge.col[edge.ind], lwd = edge.ind / lwd.adj)
 
   }
 }
 
 
-flight_network(aao[drive.SFO == 0], SFO)
 
+# Build Flight Network Plots ----------------------------------------------
+
+# col.1 <- adjustcolor("orange red", alpha = 0.10)
+# col.2 <- adjustcolor("orange", alpha = 0.15)
+# edge.pal <- colorRampPalette(c(col.1, col.2), alpha = TRUE)
+# edge.col <- edge.pal(100)
+
+# Parameterize color of lines
+plot.cols1 <- viridis(nrow(conventions), alpha = 0.30, begin = 0.35)
+plot.cols2 <- viridis(nrow(conventions), alpha = 0.35, begin = 0.4)
+
+for (i in 1:nrow(conventions)) {
+
+  edge.pal <- colorRampPalette(c(plot.cols1[i], plot.cols2[i]), alpha = TRUE)
+  edge.col <- edge.pal(100)
+
+  flight_network(
+    data = aao[get(paste0("drive.", conventions$IATA[i])) == 0], # exclude drivers
+    destination = conventions[, .(Longitude, Latitude)][i],
+    main = paste("AAO", conventions$`Airport City`[i], "Meeting Flight Network"),
+    point.col = plot.cols1[i],
+    edge.col = edge.col
+    )
+
+}
+
+
+# Legacy ------------------------------------------------------------------
+
+
+# flight_network(aao[drive.SFO == 0], SFO)
+#
+#
+#
+#
+#
+# # Get coordinates for destination airport SFO
+# SFO <- as.numeric(aao[`Airport Code` == "SFO", .(Longitude, Latitude)])
+#
+# # Plot a map of the united states
+# map("world", col = "grey20", fill = TRUE, bg = "black", lwd = 0.1)
+#
+# title(
+#   main = "AAO San Francisco Meeting Flight Network",
+#   col.main = "white", col.sub = "white"
+# )
+#
+# # Plot attendee origins (proxy via airports locations)
+# points(
+#   x = aao[drive.SFO == 0]$Longitude,
+#   y = aao[drive.SFO == 0]$Latitude,
+#   pch = 20,
+#   cex = log(aao[drive.SFO == 0]$Frequency) / (max(log(aao[drive.SFO == 0]$Frequency)) * 2),
+#   col = rgb(red = 1, green = .69, blue = 0, alpha = 0.3)
+# )
+#
+# # Parameterize color of lines
+# col.1 <- adjustcolor("orange red", alpha = 0.10)
+# col.2 <- adjustcolor("orange", alpha = 0.15)
+# edge.pal <- colorRampPalette(c(col.1, col.2), alpha = TRUE)
+# edge.col <- edge.pal(100)
+#
+#
+#
+#
+# rgb(red = 1, green = .69, blue = 0, alpha = 0.3)
 
 # Export ------------------------------------------------------------------
 dev.off()
--- Code/04 - Flight Emissions.R
+++ Code/04 - Flight Emissions.R
@@ -5,10 +5,10 @@
 
 # Setup -------------------------------------------------------------------
 rm(list = ls())
-library(httr)
-library(jsonlite)
 library(tidyverse)
 library(data.table)
+library(httr)
+library(jsonlite)
 
 path.in <- "~/AAO-climate-research/Data/"
 
@@ -16,72 +16,102 @@ path.in <- "~/AAO-climate-research/Data/"
 api.key <- readRDS("C:/Users/Alec/Documents/GAP Research/GAP Climate Research/GoClimate API/Api_key.rds" )
 
 # Import ------------------------------------------------------------------
-aao <- readRDS(paste0(path.in, "AAO_GEO_UNIQUE.RDs"))
+aao <- readRDS(paste0(path.in, "AAO_GEODISTANCE.RDs"))
+conventions <- readRDS(paste0(path.in, "AAO_CONVENTIONS.RDs"))
 
 
 # Build API Queries -------------------------------------------------------
 
 # Set Query Parameters
 seat.class <- "economy" # Assume most people will fly economy class
-destination <- "SFO"
+# destination <- "SFO"
 
 # Extract origin IATA
 origins <- aao$`Airport Code`
 
 # Initialize list
-query.list <- rep(NA, length(origins))
+query.list <- vector(mode = "list", length = nrow(conventions))
+names(query.list) <- conventions$IATA
 
-# Generate list for each round-trip query
-for (i in 1:length(origins)) {
+for (destination in conventions$IATA) {
 
-  query <- paste0("https://api.goclimate.com/v1/flight_footprint?segments[0][origin]=",
-    origins[i],          # Loop through each unique origin airport
-    "&segments[0][destination]=",
-    destination,         # Nearest airport to convention center
-    "&segments[1][origin]=",
-    destination,         # 2nd-leg of round trip flight
-    "&segments[1][destination]=",
-    origins[i],          # Return home
-    "&cabin_class=",
-    seat.class,              # Seat class of passenger
-    "&currencies[]=USD")     # Price offsets in USD
+  # Build interim query vector
+  query.vec <- rep(NA, length(origins))
 
-  query.list[i] <- query
+  # Generate list for each round-trip query
+  for (i in 1:length(origins)) {
+
+    query <- paste0("https://api.goclimate.com/v1/flight_footprint?segments[0][origin]=",
+      origins[i],          # Loop through each unique origin airport
+      "&segments[0][destination]=",
+      destination,         # Nearest airport to convention center
+      "&segments[1][origin]=",
+      destination,         # 2nd-leg of round trip flight
+      "&segments[1][destination]=",
+      origins[i],          # Return home
+      "&cabin_class=",
+      seat.class,              # Seat class of passenger
+      "&currencies[]=USD")     # Price offsets in USD
+
+    query.vec[i] <- query
+
+  }
+
+  # Put vector in list
+  query.list[[destination]] <- query.vec
 
 }
 
 
 # Query GoClimate API -----------------------------------------------------
 
-# Initialize query responses
-responses.footprint <- rep(NA, length(query.list))
-responses.offsets <- rep(NA, length(query.list))
+# Initialize list
+response.list <- vector(mode = "list", length = nrow(conventions))
+names(response.list) <- conventions$IATA
 
-# Loop through all queries in query.list
-for (i in 1:length(query.list)) {
+for (destination in conventions$IATA) {
 
-  r <- GET(query.list[i],
-    authenticate(api.key, ""))
+  # Initialize query responses
+  responses.footprint <- rep(NA, length(origins))
+  responses.offsets <- rep(NA, length(origins))
 
-  c <- fromJSON(content(r, as = "text"))
+  # Loop through all queries in query.list
+  for (i in 1:length(origins)) {
 
-  # Ignore null return values from API
-  tryCatch({
-    responses.footprint[i] <- as.numeric(c$footprint)
-    responses.offsets[i] <- as.numeric(c$offset_prices[1])
-  },
+    r <- GET(query.list[[destination]][i],
+      authenticate(api.key, ""))
 
-    error = function(e) {
-      print(paste0("NULL QUERY ", i))
-    })
+    c <- fromJSON(content(r, as = "text"))
+
+    # Ignore null return values from API
+    tryCatch({
+      responses.footprint[i] <- as.numeric(c$footprint)
+      responses.offsets[i] <- as.numeric(c$offset_prices[1])
+    },
+
+      error = function(e) {
+        print(paste0("NULL QUERY ", i))
+      })
+  }
+
+  # Coerce responses into list
+  response.list[[destination]] <- data.table(
+    "Footprint" = responses.footprint,
+    "Offset" = responses.offsets
+    )
+
+  # Be explicit in column names
+  setnames(
+    response.list[[destination]],
+    c(paste0("Footprint.", destination),
+      paste0("Offset.", destination)
+      )
+    )
+
 }
 
 # Append to Geodata
-aao.emissions <- cbind(
-  aao,
-  Footprint = responses.footprint,
-  Offset = responses.offsets
-  )
+aao.emissions <- cbind(aao, bind_cols(response.list))
 
 
 # Export ------------------------------------------------------------------
