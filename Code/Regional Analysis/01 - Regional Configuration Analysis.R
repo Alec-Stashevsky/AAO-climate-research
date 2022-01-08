84007c7cfce655efacbf937f3a5504f4801c117d
Alec Stashevsky <alec.stashevsky@protonmail.com>
2022-01-08 00:37:15 -0800

    complete regional config analysis

--- Code/Regional Analysis/01 - Regional Configuration Analysis.R
+++ Code/Regional Analysis/01 - Regional Configuration Analysis.R
@@ -8,6 +8,7 @@ rm(list = ls())
 library(tidyverse)
 library(data.table)
 library(geosphere)
+library(matrixStats)
 
 path.in <- "~/AAO-climate-research/Data/"
 # path.viz <- "~/AAO-climate-research/Visualizations/"
@@ -26,9 +27,23 @@ conventions <- readRDS(paste0(path.in, "AAO_CONVENTIONS.RDs"))
 conf.grid <- data.table(
   expand.grid(
     "West" = c("SFO", "LAS"),
-    "East" = c("ORD", "MSY", "MCO")
+    "East" = c("ORD", "MSY", "MCO"),
+    stringsAsFactors = FALSE
     )
   )
+
+# Regional Configuration Grid
+conf.grid2 <- data.table(
+  expand.grid(
+    "West" = c("SFO", "LAS"),
+    "East" = c("ORD", "MSY", "MCO"),
+    "East2" = c("ORD", "MSY", "MCO"),
+    stringsAsFactors = FALSE
+  )
+)
+
+# Dedupe
+conf.grid2 <- conf.grid2[East != East2]
 
 # Get closest meeting for each regional configuration
 aao[, `:=`(
@@ -110,3 +125,175 @@ distance.log <- arrange(
   ],
   `Aggregate Distance`
 )
+
+
+# Add Second East Coast Conference ----------------------------------------
+
+for (i in 1:nrow(conf.grid2)) {
+
+  aao[,
+    paste(
+      "regional",
+      conf.grid2$West[i],
+      conf.grid2$East[i],
+      conf.grid2$East2[i],
+      sep = "_"
+      ) := fcase(
+
+        # Test if min distance to all 3 conferences is equal to:
+
+        # West
+        get(paste0("gdist.", conf.grid2$West[i])) == rowMins(
+          as.matrix(data.frame(
+            get(paste0("gdist.", conf.grid2$West[i])),
+            get(paste0("gdist.", conf.grid2$East[i])),
+            get(paste0("gdist.", conf.grid2$East2[i]))
+            ))
+          ), conf.grid2$West[i],
+
+        # East
+        get(paste0("gdist.", conf.grid2$East[i])) == rowMins(
+          as.matrix(data.frame(
+            get(paste0("gdist.", conf.grid2$West[i])),
+            get(paste0("gdist.", conf.grid2$East[i])),
+            get(paste0("gdist.", conf.grid2$East2[i]))
+            ))
+          ), conf.grid2$East[i],
+
+        # East2
+        get(paste0("gdist.", conf.grid2$East2[i])) == rowMins(
+          as.matrix(data.frame(
+            get(paste0("gdist.", conf.grid2$West[i])),
+            get(paste0("gdist.", conf.grid2$East[i])),
+            get(paste0("gdist.", conf.grid2$East2[i]))
+            ))
+          ), conf.grid2$East2[i]
+        )
+    ]
+}
+
+distance.log2 <- data.table(
+  "Configuration" = colnames(aao)[(ncol(aao) - 11):ncol(aao)],
+  "Aggregate Distance" = rep(NA, nrow(conf.grid2))
+)
+
+
+# Calculate total geodesic distance for each coordinate in sample
+for (i in 1:nrow(conf.grid2)) {
+
+  distance.log2$`Aggregate Distance`[i] <- sum(
+
+    # West Coast ----
+
+    # Weight by frequency
+    aao[
+      get(
+        paste(
+          "regional",
+          conf.grid2$West[i],
+          conf.grid2$East[i],
+          conf.grid2$East2[i],
+          sep = "_"
+        )
+      ) == conf.grid2$West[i]
+    ]$Frequency * distGeo(
+
+      # Attendee origin
+      aao[
+        get(
+          paste(
+            "regional",
+            conf.grid2$West[i],
+            conf.grid2$East[i],
+            conf.grid2$East2[i],
+            sep = "_"
+          )
+        ) == conf.grid2$West[i],
+        .(Longitude, Latitude)
+      ],
+
+      # West coast destination
+      conventions[IATA == conf.grid2$West[i], .(Longitude, Latitude)]
+    ),
+
+
+    # East Coast ----
+
+    # Weight by frequency
+    aao[
+      get(
+        paste(
+          "regional",
+          conf.grid2$West[i],
+          conf.grid2$East[i],
+          conf.grid2$East2[i],
+          sep = "_"
+        )
+      ) == conf.grid2$East[i]
+    ]$Frequency * distGeo(
+
+      # Attendee origin
+      aao[
+        get(
+          paste(
+            "regional",
+            conf.grid2$West[i],
+            conf.grid2$East[i],
+            conf.grid2$East2[i],
+            sep = "_"
+          )
+        ) == conf.grid2$East[i],
+        .(Longitude, Latitude)
+      ],
+
+      # East coast destination
+      conventions[IATA == conf.grid2$East[i], .(Longitude, Latitude)]
+    ),
+
+    # East Coast 2 ----
+
+    # Weight by frequency
+    aao[
+      get(
+        paste(
+          "regional",
+          conf.grid2$West[i],
+          conf.grid2$East[i],
+          conf.grid2$East2[i],
+          sep = "_"
+        )
+      ) == conf.grid2$East2[i]
+    ]$Frequency * distGeo(
+
+      # Attendee origin
+      aao[
+        get(
+          paste(
+            "regional",
+            conf.grid2$West[i],
+            conf.grid2$East[i],
+            conf.grid2$East2[i],
+            sep = "_"
+          )
+        ) == conf.grid2$East2[i],
+        .(Longitude, Latitude)
+      ],
+
+      # East coast destination
+      conventions[IATA == conf.grid2$East2[i], .(Longitude, Latitude)]
+    )
+
+
+  )
+}
+
+# Add % difference and sort
+distance.log2 <- arrange(
+  distance.log2[,
+    `Percent Change` := (`Aggregate Distance` - min(`Aggregate Distance`)) / min(`Aggregate Distance`)
+  ],
+  `Aggregate Distance`
+)
+
+# Drop every other row
+distance.log2 <- distance.log2[seq(1, nrow(distance.log2), 2)]
