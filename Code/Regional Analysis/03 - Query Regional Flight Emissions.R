8047e29d740004d2d5141b95d557466c478bdcae
Alec Stashevsky <alec.stashevsky@protonmail.com>
2022-01-08 02:40:11 -0800

    init regional query code

---
+++ Code/Regional Analysis/03 - Query Regional Flight Emissions.R
@@ -0,0 +1,9 @@
+# Step 03 - Query Regional Flight Emissions
+# Alec Stashevsky
+# January 8, 2022
+
+
+# Setup -------------------------------------------------------------------
+rm(list = ls())
+library(tidyverse)
+library(data.table)
